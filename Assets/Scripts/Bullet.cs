4ca284c1098a413121a488b12239ad7ca82dfdd7
baki_12_95@mail.ru <baki_12_95@mail.ru>
2020-01-21 22:52:45 +0300

    Моя первая игра. На данном этапе создан персонаж и он может двигать, прыгать и стрелять. Также добавлено препятсвие в виде пилы, которая наносит урон, если с ним столкнуться

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed = 10.0F;
+    private Vector3 direction;
+    public Vector3 Direction { set { direction = value; } }
+
+    private SpriteRenderer Sprite;
+
+    private void Awake()
+    {
+        Sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, 1.4F);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+    public GameObject player;
+            	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+	}
+}
